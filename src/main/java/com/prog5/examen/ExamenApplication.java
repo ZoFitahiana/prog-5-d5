47b655819d4dfd3c1dfdc1631eef66a285fc42ca
ZoFitahiana <hei.tsiaroniaina@gmail.com>
2025-05-28 08:49:55 +0200

    fix: Update  main to test app with spring boot

--- src/main/java/com/prog5/examen/ExamenApplication.java
+++ src/main/java/com/prog5/examen/ExamenApplication.java
@@ -1,13 +1,40 @@
 package com.prog5.examen;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ExamenApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ExamenApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ExamenApplication.class, args);
+    }
 
+    @Bean
+    public CommandLineRunner run() {
+        return args -> {
+
+            Utilisateur utilisateur = new Utilisateur("Fifaliana", 50.0);
+
+			MachineACafe machineACafe = new MachineACafe(4, 80, true);
+
+            Paiement paiement = new PaiementImpl();
+
+            String typeCafe = utilisateur.choisirCafe("Café au lait ");
+
+            machineACafe.verifierStock();
+
+            String cafePrepare = machineACafe.preparerCafe(typeCafe);
+
+            if (cafePrepare != null) {
+                boolean paiementReussi = utilisateur.payer(paiement, 2.5);
+
+                if (paiementReussi) {
+                    utilisateur.recevoirCafe(cafePrepare);
+                }
+            }
+        };
+    }
 }
